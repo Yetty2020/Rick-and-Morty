0c34bd50d5fcdd78d963f21992cb8efc4c44cf7a
Yetty2020 <fatihaadetoro@gmail.com>
2026-03-20 22:18:26 +0100

    404 error handling

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,16 +1,26 @@
-import { useQuery } from "@apollo/client/react";
-import { GET_CHARACTERS } from "../queries";
-import type { CharactersResponse, CharactersVars } from "../types";
-import CharacterCard from "./CharacterCard";
 
-const Gallery = () => {
-  const { loading, error, data } = useQuery<CharactersResponse, CharactersVars>(GET_CHARACTERS, {
-    variables: { page: 1,
-     },
-  });
+
+
+import CharacterCard from "./CharacterCard";
+import { useCharacters } from "../useCharacters";
+
+const Gallery = ({ searchTerm }: { searchTerm: string }) => {
+  const { loading, error, data } = useCharacters(1, searchTerm)
 
   if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+
+  // to check for not found error 
+  if (error){
+    if (error.message.includes("404") || error.message.includes("not found")){
+        return <div>
+            <h3>No characters found for {searchTerm}</h3>
+            <p>Maybe they're in another dimension</p>
+        </div>
+    }
+    return 
+        <div>Portal Failure: {error.message}</div>
+    
+  }
   console.log(data)
 
   return (
