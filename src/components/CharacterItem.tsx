8bb0ff19a7126dfb0e7a0882534f09ffde135586
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-15 17:43:12 +0100

    style: Added gsap animation to characterDetail page

--- src/components/CharacterItem.tsx
+++ src/components/CharacterItem.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios, { type AxiosResponse, type AxiosError } from "axios";
 import astro from "../assets/astro.svg";
 import gun from "../assets/gun.svg";
@@ -14,6 +14,11 @@ import { GiTreasureMap } from "react-icons/gi";
 import { TiArrowBackOutline } from "react-icons/ti";
 import Loading from "./Loading";
 import CharacterError from "./CharacterError";
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(useGSAP, TextPlugin);
 
 interface CharacterItemProps {
   id: number;
@@ -43,6 +48,62 @@ function CharacterItem() {
   const [loading, setLoading] = useState(true);
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
+  const container = useRef<HTMLDivElement>(null)
+
+  useGSAP(() =>{
+    const tl = gsap.timeline()
+    const nameElement = container.current?.querySelector(".character-name") as HTMLElement;
+
+    if (!loading && character){
+gsap.from(".character-portrait", {
+  clipPath: "inset(50% 0 50% 0)", // Starts as a horizontal line in the middle
+  duration: 1,
+  ease: "power4.inOut",
+  delay: 0.5
+});
+
+
+      
+
+    tl.from(".bento-box", {
+    duration: 0.8,
+    y: 30,             
+    opacity: 0,      
+    rotationX: -15,     
+    stagger: {
+      each: 0.25,      
+      from: "start"    
+    },
+    ease: "power3.out", 
+  }, "1.5");
+
+  if (nameElement) {
+      const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%&$#@";
+      const originalName = character.name;
+      nameElement.innerText = "";
+
+      tl.to(nameElement, {
+        duration: 0.7,
+        onUpdate: function() {
+          // Generate random "alien code" characters
+          const randomText = Array.from({ length: originalName.length }, () => 
+            chars[Math.floor(Math.random() * chars.length)]
+          ).join("");
+          
+          nameElement.innerText = randomText; 
+        },
+        onComplete: () => {
+          nameElement.innerText = originalName; 
+        }
+      }, "-=1");
+    }
+
+
+    }
+}, { 
+    dependencies: [loading, character], 
+    scope: container 
+  });
 
   const statusMap = {
     Alive: {
@@ -113,7 +174,7 @@ function CharacterItem() {
   }
   return (
     <div className="min-h-screen lg:h-screen w-full flex flex-col lg:flex-row items-center justify-center p-6  border-l-2 bg-[#0D7C85] border-[#EBFF6E]  filter-[url(#ink-bleed)]  ">
-      <div className="flex flex-col lg:flex-row   gap-4 lg:gap-10 bg-white p-10 rounded-lg w-full justify-between shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] border-b-[5px] border-r-[5px] border-t-2 border-l-2 lg:h-[85vh]  border-[#EBFF6E] rotate-[0.5deg] lg:-rotate-1 md:max-w-xl lg:max-w-7xl lg:py-12 ">
+      <div className="flex flex-col lg:flex-row   gap-4 lg:gap-10 bg-white p-10 rounded-lg w-full justify-between shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] border-b-[5px] border-r-[5px] border-t-2 border-l-2 lg:h-[85vh]  border-[#EBFF6E] rotate-[0.5deg] lg:-rotate-1 md:max-w-xl lg:max-w-7xl lg:py-12 " ref={container}>
         {/* Right hand side */}
         <div className="lg:w-2/5 flex flex-col gap-4">
           <button
@@ -126,14 +187,14 @@ function CharacterItem() {
             <span>Back to Earth</span>
           </button>
 
-          <div className="">
+          <div className="character-portrait">
             <img
               src={character?.image}
               alt={character?.name}
               className="border-4 border-black rounded-xl w-full lg:w-[80%] shadow-[8px_8px_0px_0px_rgba(0,0,0,0.2)]"
             />
           </div>
-          <div className="p-4 rounded-lg  font-mono text-xs lg:flex items-center  justify-between w-full bg-[#EBFF6E] border-2 border-black  lg:px-6 lg:py-2 font-bold shadow-[4px_4px_0px_0px_#000] hidden ">
+          <div className="p-4 rounded-lg  font-mono text-xs lg:flex items-center  justify-between w-full bg-[#EBFF6E] border-2 border-black  lg:px-6 lg:py-2 font-bold shadow-[4px_4px_0px_0px_#000] hidden bento-box">
             <img src={astro} alt="astro" className="w-10  animate-pulse" />
             <img src={gun} alt="gun" className="w-10  " />
             <img src={neptune} alt="neptune" className="w-10  " />
@@ -141,12 +202,12 @@ function CharacterItem() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-5 w-full lg:w-3/5 ">
-          <div className="bg-[#407772] p-3 lg:p-4 rounded-lg border-2 border-[#EBFF6E] font-mono text-xs shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] text-[#EBFF6E] flex flex-col gap-1">
+        <div className="flex flex-col gap-5 w-full lg:w-3/5 ref={container}">
+          <div className="bg-[#407772] p-3 lg:p-4 rounded-lg border-2 border-[#EBFF6E] font-mono text-xs shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] text-[#EBFF6E] flex flex-col gap-1 bento-box">
             <p className="font-black text-xl lg:text-2xl uppercase  leading-none text-black ">
               Name
             </p>
-            <h4 className="font-black text-2xl lg:text-5xl  uppercase italic leading-none">
+            <h4 className="font-black text-2xl lg:text-5xl  uppercase italic leading-none character-name">
               {character?.name}
             </h4>
           </div>
@@ -154,7 +215,7 @@ function CharacterItem() {
           <div className=" flex flex-col gap-6">
             <div className="flex flex-col gap-5 lg:flex-row justify-between">
               <div
-                className={`flex flex-row-reverse md:items-start lg:flex-col lg:gap-6 lg:items-center gap-3 p-3 rounded-lg border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] ${currentStatus.bgColor}`}
+                className={`flex flex-row-reverse md:items-start lg:flex-col lg:gap-6 lg:items-center gap-3 p-3 rounded-lg border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] ${currentStatus.bgColor} bento-box`}
               >
                 {/* The Dynamic Text */}
                 <div className="flex flex-col lg:gap-3">
@@ -168,9 +229,9 @@ function CharacterItem() {
                   </span>
                 </div>
                 {/* The Dynamic Icon */}
-                <div className="shrink-0">{currentStatus.icon}</div>
+                <div className="shrink-0 ">{currentStatus.icon}</div>
               </div>
-              <div className=" p-4 rounded-lg border-2 border-[#EBFF6E] font-mono text-xs   shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] ">
+              <div className=" p-4 rounded-lg border-2 border-[#EBFF6E] font-mono text-xs   shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] bento-box ">
                 {/* origin vs current location */}
                 <h4 className="uppercase text-lg lg:text-2xl font-bold">
                   origin vs current location
@@ -194,8 +255,8 @@ function CharacterItem() {
             </div>
 
             <div className="flex flex-col lg:flex-row gap-6 items-start w-full">
-              <div className="p-4 rounded-lg border-2   flex flex-col gap-3 bg-[#407772]  border-[#EBFF6E] font-mono text-xs shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] text-[#EBFF6E]  w-full">
-                <h4 className="font-bold text-lg lg:text-2xl border-b border-[#EBFF6E]/30">
+              <div className="p-4 rounded-lg border-2   flex flex-col gap-3 bg-[#407772]  border-[#EBFF6E] font-mono text-xs shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] text-[#EBFF6E]  w-full bento-box">
+                <h4 className="font-bold text-lg lg:text-2xl border-b border-[#EBFF6E]/30 ">
                   Character Dossier
                 </h4>
                 <p>
@@ -212,7 +273,7 @@ function CharacterItem() {
                 </p>
               </div>
               {/* EPISODE TERMINAL */}
-              <div className="bg-black p-4 rounded-lg border-2 border-[#EBFF6E] font-mono text-xs shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] w-full">
+              <div className="bg-black p-4 rounded-lg border-2 border-[#EBFF6E] font-mono text-xs shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] w-full bento-box">
                 <p className="text-[#EBFF6E] mb-2 uppercase tracking-widest border-b border-[#EBFF6E]/30 pb-1 ">
                   Recent Logs:
                 </p>
@@ -231,7 +292,7 @@ function CharacterItem() {
             </div>
           </div>
 
-          <div className="p-4 rounded-lg  font-mono text-xs flex items-center  justify-between w-full bg-[#EBFF6E] border-2 border-black  lg:px-6 lg:py-2 font-bold shadow-[4px_4px_0px_0px_#000] lg:hidden ">
+          <div className="p-4 rounded-lg  font-mono text-xs flex items-center  justify-between w-full bg-[#EBFF6E] border-2 border-black  lg:px-6 lg:py-2 font-bold shadow-[4px_4px_0px_0px_#000] lg:hidden bento-box ">
             <img src={astro} alt="astro" className="w-10  animate-pulse" />
             <img src={gun} alt="gun" className="w-10  " />
             <img src={neptune} alt="neptune" className="w-10  " />
